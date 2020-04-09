7cbc88870f90342a7586ff2101b2c5c2a389d167
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-09 17:45:12 -0400

    chore: add Lottery component

--- 06-react-state/code/state-demo/src/Lottery.js
+++ 06-react-state/code/state-demo/src/Lottery.js
@@ -3,18 +3,38 @@ import LottoBall from './LottoBall';
 import './Lottery.css';
 
 class Lottery extends Component {
+    static defaultProps = {
+        title: 'Mega Millions',
+        maxBalls: 5,
+        maxNum: 75
+    }
+
     constructor(props) {
         super(props);
         this.state = {
-
+            nums: Array.from({ length: this.props.maxBalls })
         }
+    }
+
+    generate = () => {
+        this.setState((currState) => ({
+            nums: currState.nums.map(num => Math.floor(Math.random() * this.props.maxNum) + 1)
+        }))
+    }
+
+    handleClick = () => {
+        this.generate();
     }
 
     render() {
         return (
-            <div className="Lottery">
-                <LottoBall num={5} />
-            </div>
+            <section className="Lottery">
+                <h1>{this.props.title}</h1>
+                <div>
+                    {this.state.nums.map(num => <LottoBall num={num} />)}
+                </div>
+                <button onClick={this.handleClick}>Generate Numbers</button>
+            </section>
         )
     }
 }
