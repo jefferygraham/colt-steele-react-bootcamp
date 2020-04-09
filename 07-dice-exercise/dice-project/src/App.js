98ef82e19e507b91e9166c58bc284358291b114f
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-08 21:14:51 -0400

    chore: add Die component

---
+++ 07-dice-exercise/dice-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Die from './Die';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face='four' />
+    </div>
+  );
+}
+
+export default App;
