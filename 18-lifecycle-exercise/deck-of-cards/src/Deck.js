e4d5ed67bb61666e302649bf6bef70e9ff954b1e
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-23 20:58:54 -0400

    feat: deal card on click

--- 18-lifecycle-exercise/deck-of-cards/src/Deck.js
+++ 18-lifecycle-exercise/deck-of-cards/src/Deck.js
@@ -5,17 +5,45 @@ class Deck extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deck: ''
+            deck_id: '',
+            remaining: 52,
+            currentCard: ''
         }
     }
 
     componentDidMount() {
-        axios.get('https://deckofcardsapi.com/api/deck/new/shuffle ')
+        axios.get('https://deckofcardsapi.com/api/deck/new/shuffle')
+            .then(res => {
+                const data = res.data;
+                this.setState({
+                    deck_id: data.deck_id,
+                    remaining: data.remaining
+                })
+            })
     }
+
+
+    handleClick = () => {
+        axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/`)
+            .then(res => {
+                const data = res.data;
+                console.log(data)
+                this.setState({
+                    remaining: data.remaining,
+                    currentCard: data.cards[0].image
+                })
+            })
+    }
+
+
+
     render() {
         return (
             <div>
-                <h1>DECK</h1>
+                <button onClick={this.handleClick}>Gimme a card</button>
+                <div>
+                    <img src={this.state.currentCard} />
+                </div>
             </div>
         )
     }
