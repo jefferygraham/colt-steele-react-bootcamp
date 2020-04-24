d4fa947d792578370e51fd99d62322aa1dda91d6
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-23 22:00:06 -0400

    chore: fix displaying cards

--- 18-lifecycle-exercise/deck-of-cards/src/Card.js
+++ 18-lifecycle-exercise/deck-of-cards/src/Card.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 class Card extends Component {
     render() {
         return (
-            <img src={this.props.imgSrc} alt={`${this.props.value.toLowerCase()}-of-${this.props.suit.toLowerCase()}`} />
+
+            this.props.imgSrc !== '' && <img
+                src={this.props.imgSrc}
+                alt={`${this.props.value.toLowerCase()}-of-${this.props.suit.toLowerCase()}`}
+            />
+
         )
     }
 }
